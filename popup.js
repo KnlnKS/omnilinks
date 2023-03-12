9153de1b8344ed5bf3b086110c0b6c40a10332be
KnlnKS <soulkks@hotmail.ca>
2023-03-11 19:10:52 -0500

    🥅 Add errors

--- popup.js
+++ popup.js
@@ -1,16 +1,58 @@
+// Helper functions
+const isValidUrl = (urlString) => {
+  try {
+    return Boolean(new URL(urlString));
+  } catch (e) {
+    return false;
+  }
+};
+
+const toggleDisabled = () => {
+  document.querySelector("#submit").disabled =
+    !document.querySelector("#submit").disabled;
+  if (document.querySelector("#submit").disabled) {
+    document.querySelector("#submit").innerHTML = "Saving...";
+  } else {
+    document.querySelector("#submit").innerHTML = "Save";
+  }
+};
+
+const onError = (message, id) => {
+  document.querySelector("#formError").innerHTML = message;
+  document.querySelector(id).focus();
+};
+
 // Fill destination field with current tab URL
 browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
   document.querySelector("#destination").value = tabs[0].url;
 }, console.error);
 
+// Form to add new link
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
+  toggleDisabled();
+  document.querySelector("#formError").innerHTML = "";
 
+  // Get form values
   const destination = document.querySelector("#destination").value;
   const linkName = document.querySelector("#linkName").value;
   const description = document.querySelector("#description").value;
 
-  browser.storage.local.set({
-    [linkName]: { destination, linkName, description },
-  });
+  if (!isValidUrl(destination)) {
+    onError("Please enter a valid URL", "#destination");
+  } else if (!linkName) {
+    onError("Please enter a link name", "#linkName");
+  } else {
+    browser.storage.local.get(linkName).then((result) => {
+      if (result[linkName]) {
+        onError("This link name already exists", "#linkName");
+        return;
+      }
+      // Save link to storage
+      browser.storage.local.set({
+        [linkName]: { destination, linkName, description },
+      });
+    });
+  }
+  toggleDisabled();
 });
